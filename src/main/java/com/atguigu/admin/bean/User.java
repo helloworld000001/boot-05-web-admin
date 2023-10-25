aa8530288511b4b71a51c9fed5fe5397297c0f6d
root <2032461441@qq.com>
2023-10-25 11:51:52 +0800

    springboot-admin后台管理系统

---
+++ src/main/java/com/atguigu/admin/bean/User.java
@@ -0,0 +1,14 @@
+package com.atguigu.admin.bean;
+
+import lombok.Data;
+
+/**
+ * @auther 陈彤琳
+ * @Description $
+ * 2023/10/23 23:05
+ */
+@Data
+public class User {
+    private String userName;
+    private String password;
+}
---
+++ src/main/java/com/atguigu/admin/controller/IndexController.java
@@ -0,0 +1,67 @@
+package com.atguigu.admin.controller;
+
+import com.atguigu.admin.bean.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * @auther 陈彤琳
+ * @Description $
+ * 2023/10/23 14:12
+ */
+@Controller
+public class IndexController {
+    /**
+     * 到登录页
+     * @return
+     */
+    /* 表示匹配url是/或/login都可以 */
+    @GetMapping(value = {"/","/login"})
+    public String loginPage(){
+        // 表示跳转到templates/login.html
+        return "login";
+    }
+
+    /* 如果请求的路径是/login,就跳转到templates/index.html页面 */
+    @PostMapping("/login")
+    /* 使用User获取，会自动根据表单提交的参数封装到User对象中 */
+    public String main(User user, HttpSession session, Model model){
+
+        if((!StringUtils.isEmpty(user.getUserName())) && StringUtils.hasLength(user.getPassword())){
+            //把登录成功的用户保存起来
+            session.setAttribute("loginUser", user);
+            //使用重定向防止表单重复提交redirect
+            //此时不断刷新页面，只会刷新@GetMapping("/index.html")请求
+            //不会刷新本请求(@PostMapping("/login")),而导致表单的重复提交
+            return "redirect:/index.html";
+        }else{
+            //错误提示信息
+            model.addAttribute("msg", "账号密码错误");
+            //没有登录成功，重新回到登录页
+            return "login";
+        }
+
+
+    }
+
+    /* 里面跳转的url也会根据thymeleaf规则查找资源*/
+    @GetMapping("/index.html")
+    public String mainPage(HttpSession session, Model model){
+        //想要跳转到该页面需要判断是否登录成功，成功了才能跳转
+        Object loginUser = session.getAttribute("loginUser");
+        if(loginUser != null){
+            //真正实现thymeleaf查找index.html静态资源
+            return "index";
+        }else {
+            //错误提示信息
+            model.addAttribute("msg", "请重新登录");
+            //没有登录成功，重新回到登录页
+            return "login";
+        }
+    }
+}
