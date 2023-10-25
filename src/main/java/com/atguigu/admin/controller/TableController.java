b89c022d6346e56fd4e2d3aeb13b02d76da393c3
root <2032461441@qq.com>
2023-10-25 18:20:57 +0800

    fix:logout退出，表格显示user数据，点击Dashboard回到index页

--- src/main/java/com/atguigu/admin/controller/TableController.java
+++ src/main/java/com/atguigu/admin/controller/TableController.java
@@ -1,8 +1,12 @@
 package com.atguigu.admin.controller;
 
+import com.atguigu.admin.bean.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
 /**
  * @auther 陈彤琳
  * @Description $
@@ -16,7 +20,14 @@ public class TableController {
     }
 
     @GetMapping("/dynamic_table")
-    public String dynamic_table(){
+    public String dynamic_table(Model model){
+        //表格内容的遍历
+        List<User> users = Arrays.asList(new User("zhangsan","12"),
+                new User("lisi","123"),
+                new User("wangwu","1234"),
+                new User("haha","1245"),
+                new User("hehe","12346"));
+        model.addAttribute("users", users);
         return "table/dynamic_table";
     }
 
